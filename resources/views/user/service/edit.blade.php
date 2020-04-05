20f06e843b0ce4cf9212b4ec5889f0d6943f93a8
Antonin Netalis <antonin@netalis.fr>
2020-04-05 19:25:40 +0200

    script confirmation quand on quitte la page sans enregistrer

--- resources/views/user/service/edit.blade.php
+++ resources/views/user/service/edit.blade.php
@@ -116,4 +116,52 @@
             </div>
         @endforeach
     @endif
+    <script>
+    "use strict";
+        (() => {
+        const modified_inputs = new Set;
+        const defaultValue = "defaultValue";
+        // store default values
+        addEventListener("beforeinput", (evt) => {
+            const target = evt.target;
+            if (!(defaultValue in target || defaultValue in target.dataset)) {
+                target.dataset[defaultValue] = ("" + (target.value || target.textContent)).trim();
+            }
+        });
+        // detect input modifications
+        addEventListener("input", (evt) => {
+            const target = evt.target;
+            let original;
+            if (defaultValue in target) {
+                original = target[defaultValue];
+            } else {
+                original = target.dataset[defaultValue];
+            }
+            if (original !== ("" + (target.value || target.textContent)).trim()) {
+                if (!modified_inputs.has(target)) {
+                    modified_inputs.add(target);
+                }
+            } else if (modified_inputs.has(target)) {
+                modified_inputs.delete(target);
+            }
+        });
+        addEventListener("beforeunload", (evt) => {
+            if (modified_inputs.size) {
+                const unsaved_changes_warning = "Changes you made may not be saved.";
+                evt.returnValue = unsaved_changes_warning;
+                return unsaved_changes_warning;
+            }
+        });
+
+        function clearModifiedInputs() {
+
+        }
+
+        document.querySelectorAll('form').forEach(form => {
+            addEventListener('submit', (e) => {
+                modified_inputs.clear();
+            });
+        });
+        })();
+    </script>
 @endsection
